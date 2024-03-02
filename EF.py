519b994784f624908c770f4df5ce3139458d4ee9
edgar-maddocks <edgarmaddocks@outlook.com>
2024-03-02 16:24:35 +0000

    Update plot_ef, Add ef_data

--- EF.py
+++ EF.py
@@ -47,9 +47,42 @@ def minimize_vol(target_return, returns, cov_matrix, weight_bounds = (0., 1.)):
     
     weights = results.x
     return weights
+
+def maximize_sr(returns, cov_matrix, risk_free_rate, weight_bounds = (0., 1.)):
+    n_assets = returns.shape[0]
+    init_guess = np.repeat(1/n_assets, n_assets)
+    bounds = (weight_bounds,) * n_assets
+
+    weights_sum_one = {
+        "type" : "eq",
+        "fun" : lambda w: np.sum(w) - 1
+    }
+
+    def neg_portfolio_sr(weights, returns, cov_matrix, risk_free_rate):
+        rets = portfolio_return(weights, returns)
+        vol = portfolio_vol(weights, cov_matrix)
+
+        return -((rets - risk_free_rate) / vol)
+
+    results = minimize(neg_portfolio_sr,
+                       init_guess,
+                       args=(returns, cov_matrix, risk_free_rate, ),
+                       method="SLSQP",
+                       options={"disp" : False},
+                       constraints=(weights_sum_one),
+                       bounds=bounds)
+    
+    weights = results.x
+    return weights
     
 
-def plot_ef(data, ppy, target_return = None, n_points=2500, risk_free_rate=0, verbose=0):
+def plot_ef(data, ppy, 
+            target_return = None, plot_max_SR = True,
+            draw_cml = False, 
+            n_points=2500, risk_free_rate=0, 
+            weight_bounds=(0., 1.), verbose=0):
+    if verbose:
+        return_args = {}
     ann_rets, cov_mat = get_rets_and_cov_matrix(data, ppy)
 
     n_assets = ann_rets.shape[0]
@@ -59,18 +92,39 @@ def plot_ef(data, ppy, target_return = None, n_points=2500, risk_free_rate=0, ve
     pt_rets = [portfolio_return(w, ann_rets) for w in weights]
     pt_vol = [portfolio_vol(w, cov_mat) for w in weights]
 
-    ef = pd.DataFrame({"V" : pt_vol, "R" : pt_rets, "SR" : ((np.array(pt_rets) - risk_free_rate)/ np.array(pt_vol))})
-    
-    if isinstance(target_return, float):
-        opt_weights = minimize_vol(target_return, ann_rets, cov_mat)
-        opt_ret, opt_vol = portfolio_stats(opt_weights, ann_rets, cov_mat)
+    ef = pd.DataFrame({"V" : pt_vol, "R" : pt_rets, "SR" : ((np.array(pt_rets) - risk_free_rate)/ np.array(pt_vol))}) 
+    return_args["Efficient Frontier"] = ef
 
     fig, axs = plt.subplots(1, 1, figsize=(8, 6))
 
+    xmin = ef["V"].min()-0.1*(ef["V"].max()-ef["V"].min())
+    xmax = ef["V"].max()-0.1*(ef["V"].max()-ef["V"].min())
+    axs.set_xlim(xmin, xmax)
+
+    ymin = ef["R"].min()-0.1*(ef["R"].max()-ef["R"].min())
+    ymax = ef["R"].max()-0.1*(ef["R"].max()-ef["R"].min()) 
+    axs.set_ylim(ymin, ymax)
+    
     scat = axs.scatter(ef["V"], ef["R"], s = 5, c = ef["SR"])
+
     if isinstance(target_return, float):
-        axs.scatter(opt_vol, opt_ret, marker = "*", s = 75, c = "black")
-        axs.text(opt_vol, opt_ret, f"Minimal volatility for return of {target_return}")
+        min_vol_weights = minimize_vol(target_return, ann_rets, cov_mat, weight_bounds)
+        return_args[f"Min Vol Weights"] = min_vol_weights
+        min_vol_ret, min_vol_vol = portfolio_stats(min_vol_weights, ann_rets, cov_mat)
+        axs.scatter(min_vol_vol, min_vol_ret, marker = "*", s = 75, c = "black")
+        axs.text(min_vol_vol, min_vol_ret, f"Minimal volatility for return of {target_return}")
+    if plot_max_SR:
+        max_sr_weights = maximize_sr(ann_rets, cov_mat, risk_free_rate, weight_bounds)
+        return_args["Max SR weights"] = max_sr_weights
+        max_sr_ret, max_sr_vol = portfolio_stats(max_sr_weights, ann_rets, cov_mat)
+        axs.scatter(max_sr_vol, max_sr_ret, marker = "*", s = 75, c = "black")
+        axs.text(max_sr_vol, max_sr_ret, f"Max SR portfolio")
+        if draw_cml:
+            cml_x = [0, max_sr_vol]
+            cml_y = [risk_free_rate, max_sr_ret]
+            axs.plot(cml_x, cml_y, c = "red", label = "Capital Market Line", linestyle="dashed")
+            axs.legend()
+
     axs.set_xlabel("Volatility")
     axs.set_ylabel("Returns")
     axs.set_title("Efficient Frontier")
@@ -78,10 +132,34 @@ def plot_ef(data, ppy, target_return = None, n_points=2500, risk_free_rate=0, ve
 
     plt.show()
 
-    if verbose and isinstance(target_return, float):
-        return ef, opt_weights
-    elif verbose:
-        return ef
+    if verbose:
+        return return_args
+    
+def ef_data(data, ppy, 
+            target_return = None, risk_free_rate=0, 
+            n_points=2500, weight_bounds=(0., 1.)):
+        
+    return_args = {}
+    ann_rets, cov_mat = get_rets_and_cov_matrix(data, ppy)
+
+    n_assets = ann_rets.shape[0]
+    weights = np.random.rand(n_points, n_assets)
+    weights = [x/sum(x) for x in weights]
+
+    pt_rets = [portfolio_return(w, ann_rets) for w in weights]
+    pt_vol = [portfolio_vol(w, cov_mat) for w in weights]
+
+    ef = pd.DataFrame({"V" : pt_vol, "R" : pt_rets, "SR" : ((np.array(pt_rets) - risk_free_rate)/ np.array(pt_vol))}) 
+    return_args["Efficient Frontier"] = ef
+
+
+    if isinstance(target_return, float):
+        min_vol_weights = minimize_vol(target_return, ann_rets, cov_mat, weight_bounds)
+        return_args[f"Min Vol Weights"] = min_vol_weights
+    max_sr_weights = maximize_sr(ann_rets, cov_mat, risk_free_rate, weight_bounds)
+    return_args["Max SR weights"] = max_sr_weights
+
+    return return_args
 
 data = pd.read_csv("ind30_m_ew_rets.csv")
 data = data.set_index("Unnamed: 0")
@@ -90,7 +168,9 @@ data.columns = data.columns.str.strip()
 data = data / 100
 data = data[["Food", "Beer"]]
 
-ef_data, opt_weights = plot_ef(data, 12, target_return=0.129, risk_free_rate=0.05, verbose=1)
+response = plot_ef(data, 12, risk_free_rate=0.05, verbose=1)
+
+print(response)
 
 
 
