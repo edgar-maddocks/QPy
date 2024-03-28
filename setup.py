0ed88e99e8bd4b6c562912a66e56fc4d94540db3
edgar-maddocks <edgarmaddocks@outlook.com>
2024-03-28 16:08:20 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "QPy - Quant finance library"
+LONG_DESCRIPTION = "An all-in-one quantitative finance library which aims to implement ML and traditional quantitative methods"
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="QPy",
+    version=VERSION,
+    author="Edgar Maddocks",
+    author_email="edgarmaddocks@outlook.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "termtables",
+        "tqdm",
+        "numpy",
+        "yfinance",
+        "scipy",
+        "matplotlib",
+        "statsmodels",
+        "scikit-learn",
+    ],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+    keywords=[
+        "python",
+        "first package",
+        "quant",
+        "quantitative",
+        "finance",
+        "backtesting",
+        "portfolio",
+        "stats",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
